928ad77437ab2ba9c31cdf44597d1094648ed8df
Michael Oldacre <mloldacre@live.com>
2020-07-31 12:48:52 -0400

    extra save

--- src/SearchAndFilterField/SearchAndFilterField.js
+++ src/SearchAndFilterField/SearchAndFilterField.js
@@ -3,8 +3,22 @@ import './SearchAndFilterField.css';
 
 export default class SearchAndFilterField extends React.Component {
   render() {
-    return <header>
-      Input Fields Here
-    </header>
+    return (
+      <form className='searchFilterForm'>
+        {/* Text Box */}
+        <label htmlFor='search-input'>Search:</label>
+        <input id="search-input" type="text" name="search-input" />
+        {/* Dropbox */}
+        <label htmlFor='search-filter'>Catergory:</label>
+        <select name="search-filter" size="1">
+          <option value="people">People</option>
+          <option value="films">Films</option>
+          <option value="planets">Planets</option>
+          <option value="species">Species</option>
+          <option value="starships">Starships</option>
+          <option value="vehicles">Vehicles</option>
+        </select>
+        <button>Search</button>        
+      </form>)
   }
 }
